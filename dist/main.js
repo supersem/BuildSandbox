56b966092ce8633ad9acf55c2a4f30a47c666e1e
supersem <xsupersemx@gmail.com>
2020-05-02 16:17:01 +0300

    add loader config

--- dist/main.js
+++ dist/main.js
@@ -106,7 +106,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _log__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./log */ \"./src/log.js\");\n/* harmony import */ var _calc__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./calc */ \"./src/calc.js\");\n/* harmony import */ var _react_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./react.png */ \"./src/react.png\");\n\n\n\n\n\nconst calc = new _calc__WEBPACK_IMPORTED_MODULE_1__[\"default\"]();\nconst log = new _log__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\n\nlog.log(calc.add(1, 2, 3));\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _log__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./log */ \"./src/log.js\");\n/* harmony import */ var _calc__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./calc */ \"./src/calc.js\");\n/* harmony import */ var _react_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./react.png */ \"./src/react.png\");\n\n\n\n\n\nconst calc = new _calc__WEBPACK_IMPORTED_MODULE_1__[\"default\"]();\nconst log = new _log__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\n\nlog.log(calc.add(1, 2, 3));\n\nconst el = document.createElement('img');\nel.src = _react_png__WEBPACK_IMPORTED_MODULE_2__[\"default\"];\ndocument.body.appendChild(el);\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
@@ -130,7 +130,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"5a06fd3e0e11843005af9fb26decbceb.png\");\n\n//# sourceURL=webpack:///./src/react.png?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/react-83a3f88.png\");\n\n//# sourceURL=webpack:///./src/react.png?");
 
 /***/ })
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,31 @@ module.exports = {
 
   module: {
     rules: [
+      // Loading images
       {
-        test: /\.png$/,
-        use: [{ loader: 'file-loader' }]
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }
+        ]
+      },
+      // Loading fonts
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]'
+            }
+          }
+        ]
       }
     ]
   }
