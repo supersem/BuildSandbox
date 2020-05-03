ebea198eeef6402b92f799f7caa12b72c4bb12db
supersem <xsupersemx@gmail.com>
2020-05-03 13:45:36 +0300

    config production

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.scss';
 
-const App = () => <p>This is webpackServer</p>
+const App = () => <p>This is Webpack React App</p>
 
 ReactDOM.render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,102 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: "development",
+module.exports = (env = {}) => {
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader'
-          }
-        ]
-      },
-      // Loading images
-      {
-        test: /\.(png|jpg|jpeg|gif|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name]-[sha1:hash:7].[ext]'
+  const { mode = 'development' } = env;
+
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+      'css-loader'
+    ];
+  };
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: 'Hello World!',
+        buildTime: new Date().toISOString(),
+        template: 'public/index.html'
+      })
+    ];
+
+    if (isProd) {
+      plugins.push(new MiniCssExtractPlugin({
+          filename: 'main-[hash:8].css'
+        })
+      );
+    }
+
+    return plugins;
+  }
+
+  return {
+    mode: isProd ? 'production': isDev && 'development',
+
+    output: {
+      filename: isProd ? 'main-[hash:8].js' : undefined
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader'
             }
-          }
-        ]
-      },
-      // Loading fonts
-      {
-        test: /\.(ttf|otf|eot|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]'
+          ]
+        },
+        // Loading images
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
             }
-          }
-        ]
-      },
+          ]
+        },
+        // Loading fonts
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
+            }
+          ]
+        },
 
-      // Loading css
-      {
-        test: /\.css$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' }
-        ]
-      },
+        // Loading css
+        {
+          test: /\.css$/,
+          use: getStyleLoaders()
+        },
 
-      // Loading sass/scss
-      {
-        test: /\.s[ca]ss$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-      }
-    ]
-  },
+        // Loading sass/scss
+        {
+          test: /\.s[ca]ss$/,
+          use: [ ...getStyleLoaders(), 'sass-loader' ]
+        }
+      ]
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Hello World!',
-      buildTime: new Date().toISOString(),
-      template: 'public/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'main-[hash:8].css'
-    })
-  ],
+    plugins: getPlugins(),
 
-  devServer: {
-    open: true
+    devServer: {
+      open: true
+    }
   }
 };
